dab3c450090543e3f24158f5f76d13165ca5c1ff
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-11-04 18:02:34 -0500

    Add files via upload

---
+++ public/js/lists/categoryList.mjs
@@ -0,0 +1,18 @@
+let categoryList = [ 
+     "Beef",
+    "Breakfast",
+    "Chicken",
+    "Dessert",
+    "Goat",
+    "Lamb",
+    "Miscellaneous",
+    "Pasta",
+    "Pork",
+    "Seafood",
+    "Side",
+    "Starter",
+    "Vegan",
+    "Vegetarian"
+];
+
+export { categoryList };
